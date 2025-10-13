c7032dd39b9070ffcc91aaf68a684f0d1a63e8a5
Adis <aliewafa95@gmail.com>
2025-10-13 17:12:45 +0700

    iya

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_toko"    ;
+$user = "xirpl1-16";
+$pass = "0095372394";
+$db   = "db_xirpl1-16_2"    ;
 
 $result = mysqli_connect($host, $user, $pass, $db);
 
